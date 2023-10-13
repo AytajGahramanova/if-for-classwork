eff140bc2dc43725c47bba1e0773430598db739b
AytacQehremanova <qehremanova.aytac2003@gmail.com>
2023-10-13 13:26:41 +0400

    task added

---
+++ main.js
@@ -0,0 +1,39 @@
+// TASK - 1 //
+// let proses = prompt("Emeliyyati daxil edin:");
+// let num1 = prompt("Ilkin ededi daxil edin:");
+// let num2 = prompt("Diger ededi daxil edin:");
+
+// if(proses == "toplama"){
+//     console.log(Number(num1) + Number(num2));
+// }
+// else if(proses == "cixma"){
+//     console.log(Number(num1) - Number(num2));
+// }
+// else if(proses == "vurma"){
+//     console.log(Number(num1) * Number(num2));
+// }
+// else if(proses == "bolme"){
+//     console.log(Number(num1) / Number(num2));
+// }
+
+
+// TASK - 2 //h
+// let num = parseInt(prompt("ededi daxil edin:"));
+// let counter = 0;
+// if(num==1){
+//     console.log("ne sade ne murekkeb");
+// }else{
+//     for (let i = 0; i<= Number; i++);
+// }
+
+
+// TASK - 3 //
+// let eded = parseInt(prompt("Ededi daxil edin:"));
+// let n = parseInt(prompt("Ededin quvvetini daxil edin:"));
+// let result = 1;
+// for(i = 0; i < n; i++){
+//     result = eded**n;
+// }
+// console.log(result);
+
+
